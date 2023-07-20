1df01755ead019130710afba00e55a80d41ab544
Lade06 <oladunjoyeololade@gmail.com>
2023-07-20 12:55:08 +0100

    modefied app.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import "./App.css";
 import images1 from "./Assets/a1.jpg";
 import images2 from "./Assets/bag2.jpg";
 import images3 from "./Assets/addidas.jpg";
-import images4 from "./Assets/shoe.jpg";
-import images5 from "./Assets/lappy.jpg";
-import {FaCartPlus} from "react-icons/fa6";
+// import images4 from "./Assets/shoe.jpg";
+// import images5 from "./Assets/lappy.jpg";
+import { FaCartPlus } from "react-icons/fa6";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0); //innitial state
+  const handleCount = () =>{
+    setCounter(prevCounter => prevCounter + 1)
+  }
   return (
     <div>
       {
         // this is navbar section
         <div>
-          <nav className="navbar navbar-expand-sm navbar-dark bg-dark navbar-fixed-top">
+          <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             <div className="container-fluid">
               <a className="navbar-brand" href="/">
-                <h1><span>S</span>hop<span>N</span>ow</h1>
+                <h1>
+                  <span>S</span>hop<span>N</span>ow
+                </h1>
               </a>
               <button
                 className="navbar-toggler"
@@ -45,6 +52,10 @@ function App() {
                   </li>
                 </ul>
                 <form className="d-flex">
+                  <div className="biderCount">
+                    <span className="text-white">{counter}</span>
+                    <FaCartPlus className="mr-3" color="white" size="3em" />
+                  </div>
                   <input
                     className="form-control me-2"
                     type="text"
@@ -75,73 +86,161 @@ function App() {
                         WELCOME TO <br /> WOMEN DEVELOPMENT CENTER
                       </h1>
                       <button className="btn btn-primary">Explore Now</button>
-                      <div className="text-center mt-5 text-black">
-                        <h1 className="store">SHOP</h1> 
-                        <FaCartPlus size="3em" color="blue"  className="iconn "/>
-                        
-                      </div>
-                      <div className="pic text-black">
-                      <div>
-                          <img src={images1} alt="" className="pic" />
-                          <p>
-                            Smart Backpack Backpack Set <br /> Laptop Bag
-                          </p>
-                          <p>#19,999</p>
-                          <button className="btn btn-primary">Buy Now</button>
-                        </div>
-
-                        <div>
-                          <img src={images2} alt="" className="pic" />
-                          <p>
-                            EILIFINTE 3 IN 1 Unisex Bag <br /> Backpack Set
-                          </p>
-                          <p>#10,000</p>
-                          <button className="btn btn-primary">Buy Now</button>
-                        </div>
-
-                        <div>
-                          <img src={images3} alt="" className="pic" />
-                          <p>
-                          ADIDAS Core Sneakers Grand Court Base
-                          </p>
-                          <p>#10,000</p>
-                          <button className="btn btn-primary">Buy Now</button>
-                        </div>
-
-                        <div>
-                          <img src={images2} alt="" className="pic" />
-                          <p>EILIFINTE 3 IN 1 Unisex Bag <br /> Backpack Set</p>
-                          <p>#28,000</p>
-                          <button className="btn btn-primary">Buy Now</button>
-                      </div>
-
-                      <div>
-                          <img src={images4} alt="" className="pic" />
-                          <p>Elegant Black Leather Shoe For Men</p>
-                          <p>#43,000</p>
-                          <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                        
-                      <div>
-                          <img src={images5} alt="" className="pic" />
-                          <p>Hp Stream 11 Intel Celeron 4GB RAM- 64GB <br /></p>
-                          <p>#95,900</p>
-                          <button className="btn btn-primary">Buy Now</button>
-                        </div>
-
                     </div>
                   </div>
                   <div className="col-md-2"></div>
                 </div>
               </div>
-              </div>
             </section>
           </div>
         </div>
       }
-  
+      {
+        <section>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-3">
+                <div>
+                  <div>
+                    <img width="100%" src={images1} alt="" className="pic" />
+                    <p>
+                      Smart Backpack Backpack Set <br /> Laptop Bag
+                    </p>
+                    <p>#19,999</p>
+                    <button onClick={handleCount } className="btn btn-primary">Buy Now</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div>
+                  <div>
+                    <img src={images2} alt="" className="pic" />
+                    <p>
+                      EILIFINTE 3 IN 1 Unisex Bag <br /> Backpack Set
+                    </p>
+                    <p>#10,000</p>
+                    <button onClick={ handleCount} className="btn btn-primary">Buy Now</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div>
+                  <div>
+                    <img src={images3} alt="" className="pic" />
+                    <p>ADIDAS Core Sneakers Grand Court Base</p>
+                    <p>#10,000</p>
+                    <button onClick={ handleCount}  className="btn btn-primary">Buy Now</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div>
+                  <img src={images2} alt="" className="pic" />
+                  <p>
+                    EILIFINTE 3 IN 1 Unisex Bag <br /> Backpack Set
+                  </p>
+                  <p>#28,000</p>
+                  <button onClick={ handleCount}  className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-3">
+                <div>
+                  <div>
+                    <img width="100%" src={images1} alt="" className="pic" />
+                    <p>
+                      Smart Backpack Backpack Set <br /> Laptop Bag
+                    </p>
+                    <p>#19,999</p>
+                    <button className="btn btn-primary">Buy Now</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div>
+                  <div>
+                    <img src={images2} alt="" className="pic" />
+                    <p>
+                      EILIFINTE 3 IN 1 Unisex Bag <br /> Backpack Set
+                    </p>
+                    <p>#10,000</p>
+                    <button className="btn btn-primary">Buy Now</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div>
+                  <div>
+                    <img src={images3} alt="" className="pic" />
+                    <p>ADIDAS Core Sneakers Grand Court Base</p>
+                    <p>#10,000</p>
+                    <button className="btn btn-primary">Buy Now</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div>
+                  <img src={images2} alt="" className="pic" />
+                  <p>
+                    EILIFINTE 3 IN 1 Unisex Bag <br /> Backpack Set
+                  </p>
+                  <p>#28,000</p>
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-3">
+                <div>
+                  <div>
+                    <img width="100%" src={images1} alt="" className="pic" />
+                    <p>
+                      Smart Backpack Backpack Set <br /> Laptop Bag
+                    </p>
+                    <p>#19,999</p>
+                    <button className="btn btn-primary">Buy Now</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div>
+                  <div>
+                    <img src={images2} alt="" className="pic" />
+                    <p>
+                      EILIFINTE 3 IN 1 Unisex Bag <br /> Backpack Set
+                    </p>
+                    <p>#10,000</p>
+                    <button className="btn btn-primary">Buy Now</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div>
+                  <div>
+                    <img src={images3} alt="" className="pic" />
+                    <p>ADIDAS Core Sneakers Grand Court Base</p>
+                    <p>#10,000</p>
+                    <button className="btn btn-primary">Buy Now</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div>
+                  <img src={images2} alt="" className="pic" />
+                  <p>
+                    EILIFINTE 3 IN 1 Unisex Bag <br /> Backpack Set
+                  </p>
+                  <p>#28,000</p>
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      }
     </div>
   );
-}
+};
 
 export default App;
